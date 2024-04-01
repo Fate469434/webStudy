9121adfd22efd6565878253c497a02c0033fdc13
Yu <469434916@qq.com>
2024-04-02 00:22:22 +0800

    初始vue脚手架

---
+++ 8-Vue2/01-vue_basic/20_procreatetest/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import Student from "./components/Student.vue";
+import School  from "./components/School.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 8-Vue2/01-vue_basic/20_procreatetest/src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>学生名称：{{ name }}</h2>
+    <h2>学生地址：{{ address }}</h2>
+    <button @click="showName">点我提示学生名</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'StuDent',
+    data: function () {
+      return {
+        name: "李华",
+        address: "苏高院",
+      };
+    },
+    methods: {
+      showName:function(){
+        alert(this.name)
+      }
+    },
+  };
+</script>
